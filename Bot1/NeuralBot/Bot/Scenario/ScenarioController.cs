a3cef2e9cf674e9cf1cd8d347854f7aa141ff5bb
Michael Strand <32778224+MichaelLykkeStrand@users.noreply.github.com>
2021-11-18 11:27:29 +0100

    Random stuff :) Needs a bit of refactoring

--- Bot1/NeuralBot/Bot/Scenario/ScenarioController.cs
+++ Bot1/NeuralBot/Bot/Scenario/ScenarioController.cs
@@ -1,4 +1,5 @@
-﻿using RLBotDotNet.GameState;
+﻿using Bot.Objects;
+using RLBotDotNet.GameState;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,7 +14,7 @@ namespace Bot.Scenario
         public event EventHandler OnPlayScenario;
         public GameState currentState;
         private Bot _bot;
-        Random random = new Random();
+        
         public ScenarioController(Bot bot)
         {
             this._bot = bot;
@@ -23,27 +24,19 @@ namespace Bot.Scenario
         {
             //Do scenario generation
             GameState gameState = new GameState();
-            CarState carState = new CarState
-            {
-                Boost = (float)random.NextDouble() * 100,
-                PhysicsState = new PhysicsState(
-                    location: new DesiredVector3((float)random.NextDouble()*2000, (float)random.NextDouble()*2000, 0),
-                    velocity: new DesiredVector3(100, null, 100),
-                    rotation: new DesiredRotator(0, (float)Math.PI / 2, 0),
-                    angularVelocity: new DesiredVector3(0, 0, 0)
-                )
-            };
+
 
             BallState ballState = new BallState
             {
                 PhysicsState = new PhysicsState(
-                    location: new DesiredVector3((float)random.NextDouble() * 2000, (float)random.NextDouble() * 2000, 0),
+                    location: new DesiredVector3(Field.GetRandomPosition()),
                     velocity: new DesiredVector3(null, null, 500),
                     rotation: new DesiredRotator((float)Math.PI / 2, 0, 0),
                     angularVelocity: new DesiredVector3(0, 0, 0)
                 )
             };
-            gameState.SetCarState(_bot.Index,carState);
+            gameState.SetCarState(_bot.Index,ScenarioUtils.GetRandomCarState());
+            gameState.BallState = ballState;
 
             gameState.GameInfoState.Paused = true;
 
---
+++ Bot1/NeuralBot/Bot/Scenario/ScenarioUtils.cs
@@ -0,0 +1,30 @@
+﻿using Bot.Objects;
+using RLBotDotNet;
+using RLBotDotNet.GameState;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bot.Scenario
+{
+    public static class ScenarioUtils
+    {
+        private static Random random = new Random();
+        public static CarState GetRandomCarState()
+        {
+            float rotation = random.Next(0, (int)Math.PI * 10000) / 10000;
+            CarState carState = new CarState
+            {
+                PhysicsState = new PhysicsState(
+                location: new DesiredVector3(Field.GetRandomPosition()),
+                velocity: new DesiredVector3(100, null, 100),
+                rotation: new DesiredRotator(0, rotation, 0),
+                angularVelocity: new DesiredVector3(0, 0, 0)
+    )
+            };
+            return carState;
+        }
+    }
+}
